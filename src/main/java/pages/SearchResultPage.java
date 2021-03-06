02a8fa4fea869dce14e89cd19584ff9d32af3565
rismayil <79716476+rismayil@users.noreply.github.com>
2021-03-06 16:14:25 -0600

    Added new Webelements into SearchResultsPage(maxPriceFilter,makeFilter,modelFilter,usedFilter,newRadioButton,usedRadioButton,newFilter,touring8PassengerTrim,touring8PassengerTrimFilter,listings) Added verifyFiltersDisplayed,verifyNewFilterDisplayedUsedFilterNotDisplayed,touring8PassengerTrimFilterIsDisplayed methods into SearchResultPage to verify filters displayed

--- src/main/java/pages/SearchResultPage.java
+++ src/main/java/pages/SearchResultPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,8 +20,57 @@ public class SearchResultPage extends BasePage {
 	@FindBy(css=".header-primary__title-row .srp-header")
 	public WebElement searchHeader;
 	
-	public void verifySearchHeader(String expectedHeader) {
+	
+	@FindBy(xpath="//header[@class='filter-header']//label[contains(text(),'Maximum Price: $50,000')]")
+	public WebElement maxPriceFilter;
+	
+	@FindBy(xpath="//header[@class='filter-header']//label[contains(text(),'Honda')]")
+	public WebElement makeFilter;
+	
+	@FindBy(xpath="//header[@class='filter-header']//label[contains(text(),'Pilot')]")
+	public WebElement modelFilter;
+	
+	@FindBy(xpath="//header[@class='filter-header']//label[contains(text(),'Used')]")
+	public WebElement usedFilter;
+	
+	@FindBy(xpath="//label[normalize-space()='New']")
+	public WebElement newRadioButton;
+	
+	@FindBy(xpath="//label[normalize-space()='Used']")
+	public WebElement usedRadioButton;
+	
+	@FindBy(xpath="//header[@class='filter-header']//label[contains(text(),'New')]")
+	public WebElement newFilter;
+	
+	@FindBy(xpath="//label[normalize-space()='Touring 8-Passenger']")
+	public WebElement touring8PassengerTrim;
+	
+	@FindBy(xpath="//header[@class='filter-header']//label[contains(text(),'Touring 8-Passenger')]")
+	public WebElement touring8PassengerTrimFilter;
+	
+	@FindBy(css="#srp-listing-rows-container .shop-srp-listings__listing-container")
+	public List<WebElement> listings;
+	
+	public void verifyFiltersDisplayed() {
 		//Assert.assertEquals(searchHeader, expectedHeader);
+		Assert.assertTrue(maxPriceFilter.isDisplayed());
+		Assert.assertTrue(makeFilter.isDisplayed());
+		Assert.assertTrue(modelFilter.isDisplayed());
+		Assert.assertTrue(usedFilter.isDisplayed());
 	}
+	
+	public void verifyNewFilterDisplayedUsedFilterNotDisplayed() {
+		newRadioButton.click();
+		Assert.assertTrue(newFilter.isDisplayed());
+		Assert.assertFalse(usedFilter.isDisplayed());
+	}
+	
+	public void touring8PassengerTrimFilterIsDisplayed() {
+		
+		touring8PassengerTrim.click();
+		Assert.assertTrue(touring8PassengerTrimFilter.isDisplayed());
+	}
+
+
 
 }
